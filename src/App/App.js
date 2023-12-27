df37b63da24ec086f688a11c5e6c355691dca7db
Malika <malikasenova@gmail.com>
2023-12-26 22:23:34 -0800

    Add div numbers

---
+++ src/App/App.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div class='"drag-list'>
+        <div class="drag-item" draggable="true">1</div>
+        <div class="drag-item" draggable="true">2</div>
+        <div class="drag-item" draggable="true">3</div>
+        <div class="drag-item" draggable="true">4</div>
+        <div class="drag-item" draggable="true">5</div>
+        <div class="drag-item" draggable="true">6</div>
+        <div class="drag-item" draggable="true">7</div>
+        <div class="drag-item" draggable="true">8</div>
+        <div class="drag-item" draggable="true">9</div>
+        <div class="drag-item" draggable="true">10</div>
+        <div class="drag-item" draggable="true">11</div>
+        <div class="drag-item" draggable="true">12</div>
+        <div class="drag-item" draggable="true">13</div>
+        <div class="drag-item" draggable="true">14</div>
+        <div class="drag-item" draggable="true">15</div>
+        <div class="drag-item" draggable="true">E</div>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
