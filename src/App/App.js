211c54ca5490f03f9cd117c9785f2f107f1e4438
Malika <malikasenova@gmail.com>
2024-01-12 12:31:32 -0800

    add css

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,28 @@
 import * as React from "react";
 
+import './styles.scss';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <div class='"drag-list'>
-        <div class="drag-item" draggable="true">1</div>
-        <div class="drag-item" draggable="true">2</div>
-        <div class="drag-item" draggable="true">3</div>
-        <div class="drag-item" draggable="true">4</div>
-        <div class="drag-item" draggable="true">5</div>
-        <div class="drag-item" draggable="true">6</div>
-        <div class="drag-item" draggable="true">7</div>
-        <div class="drag-item" draggable="true">8</div>
-        <div class="drag-item" draggable="true">9</div>
-        <div class="drag-item" draggable="true">10</div>
-        <div class="drag-item" draggable="true">11</div>
-        <div class="drag-item" draggable="true">12</div>
-        <div class="drag-item" draggable="true">13</div>
-        <div class="drag-item" draggable="true">14</div>
-        <div class="drag-item" draggable="true">15</div>
-        <div class="drag-item" draggable="true">E</div>
+        <div className="drag-item" draggable="true">1</div>
+        <div className="drag-item" draggable="true">2</div>
+        <div className="drag-item" draggable="true">3</div>
+        <div className="drag-item" draggable="true">4</div>
+        <div className="drag-item" draggable="true">5</div>
+        <div className="drag-item" draggable="true">6</div>
+        <div className="drag-item" draggable="true">7</div>
+        <div className="drag-item" draggable="true">8</div>
+        <div className="drag-item" draggable="true">9</div>
+        <div className="drag-item" draggable="true">10</div>
+        <div className="drag-item" draggable="true">11</div>
+        <div className="drag-item" draggable="true">12</div>
+        <div className="drag-item" draggable="true">13</div>
+        <div className="drag-item" draggable="true">14</div>
+        <div className="drag-item" draggable="true">15</div>
+        <div>E</div>
        </div>
       </header>
     </div>
