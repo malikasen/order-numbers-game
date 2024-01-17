8455353f3c54b777e6b1d3a2d39fde30b4d26c00
Malika <malikasenova@gmail.com>
2024-01-17 11:20:42 -0800

    add for loop for tiles

--- src/App/App.js
+++ src/App/App.js
@@ -3,43 +3,20 @@ import * as React from "react";
 import './styles.scss';
 
 function App() {
+  let tiles = []
+  for (let i = 0; i < 16; i++) {
+    tiles.push(
+      <div className="drag-item" draggable="true" key={i}>
+        <p className="tile-number">{i+1}</p>
+      </div>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
       <div class="left-half">
         <div class='drag-list'>
-          <div className="drag-item" draggable="true"><p className="tile-number">1</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">2</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">3</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">4</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">5</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">6</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">7</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">8</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">9</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">10</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">11</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">12</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">13</p>
-            </div>
-          <div className="drag-item" draggable="true"><p className="tile-number">14</p>
-            </div>
-            <div className="drag-item" draggable="true">
-            <p className="tile-number">15</p>
-            </div>
-          <div className="drag-item"><p className="tile-number">E</p></div>
+          {tiles}
         </div>
       </div>
       </header>
