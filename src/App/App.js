d82fb574566e45bc21080c4b63fa49eeb0a5fb09
Malika <malikasenova@gmail.com>
2024-01-17 11:02:58 -0800

    add tile color, border and center numbers

--- src/App/App.js
+++ src/App/App.js
@@ -6,24 +6,42 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <div class='"drag-list'>
-        <div className="drag-item" draggable="true">1</div>
-        <div className="drag-item" draggable="true">2</div>
-        <div className="drag-item" draggable="true">3</div>
-        <div className="drag-item" draggable="true">4</div>
-        <div className="drag-item" draggable="true">5</div>
-        <div className="drag-item" draggable="true">6</div>
-        <div className="drag-item" draggable="true">7</div>
-        <div className="drag-item" draggable="true">8</div>
-        <div className="drag-item" draggable="true">9</div>
-        <div className="drag-item" draggable="true">10</div>
-        <div className="drag-item" draggable="true">11</div>
-        <div className="drag-item" draggable="true">12</div>
-        <div className="drag-item" draggable="true">13</div>
-        <div className="drag-item" draggable="true">14</div>
-        <div className="drag-item" draggable="true">15</div>
-        <div>E</div>
-       </div>
+      <div class="left-half">
+        <div class='drag-list'>
+          <div className="drag-item" draggable="true"><p className="tile-number">1</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">2</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">3</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">4</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">5</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">6</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">7</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">8</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">9</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">10</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">11</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">12</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">13</p>
+            </div>
+          <div className="drag-item" draggable="true"><p className="tile-number">14</p>
+            </div>
+            <div className="drag-item" draggable="true">
+            <p className="tile-number">15</p>
+            </div>
+          <div className="drag-item"><p className="tile-number">E</p></div>
+        </div>
+      </div>
       </header>
     </div>
   );
