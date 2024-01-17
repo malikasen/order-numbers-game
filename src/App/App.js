c07f4d4077e5b4579c70e9e30afa122a5b607c7d
Malika <malikasenova@gmail.com>
2024-01-17 11:41:53 -0800

    format an empty tile

--- src/App/App.js
+++ src/App/App.js
@@ -4,17 +4,25 @@ import './styles.scss';
 
 function App() {
   let tiles = []
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 15; i++) {
     tiles.push(
       <div className="drag-item" draggable="true" key={i}>
         <p className="tile-number">{i+1}</p>
       </div>
     )
   }
+  tiles.push(
+    <div className="drag-item" draggable="true" id="empty" key="empty"></div>
+  )
   return (
     <div className="App">
       <header className="App-header">
-      <div class="left-half">
+      <div className="left-half">
+      <button className="button">
+          <p>
+            play
+          </p>
+        </button>
         <div class='drag-list'>
           {tiles}
         </div>
