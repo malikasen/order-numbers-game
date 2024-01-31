c92f880fd16bdcc6050b551a4029d2bab9828022
Malika <malikasenova@gmail.com>
2024-01-31 11:52:49 -0800

    add canMoveTiles function

--- src/App/App.js
+++ src/App/App.js
@@ -46,9 +46,50 @@ function App() {
         tilesInProgress[currentIndex],
       ];
     }
+    console.log('shuffled tiles', tilesInProgress);
     setShuffledTiles(tilesInProgress);
     setIsTilesShuffled(true);
   }, [tiles]);
+
+  const switchTiles = useCallback(() => {}, []);
+
+  const canMoveTile = (tileCoordinate, emptyTileCoordinate) => {
+    // Corner Cases
+    // 0'th element can move right or down
+    // 3'rd element can move left or down
+    // 12'th element can move right or up
+    // 15'th element can move left or up
+    // Edge Cases
+    // 1's and 2'nd move right, left or down
+    // 4'th and 8'th move right, up or down
+    // 7'th and 11'th move left, up or down
+    // 13'th and 14'th move left, right or up
+    // Center
+    // 5'th, 6ht, 9'th and 10'th can move left. right, up or down
+    const allowedMoveCoordinates = [
+      { 0: [1, 4] },
+      { 3: [2, 7] },
+      { 12: [13, 8] },
+      { 15: [14, 11]},
+      { 1: [0, 2, 5] },
+      { 2: [1, 3, 6] },
+      { 4: [5, 0, 8] },
+      { 8: [4, 12, 9] },
+      { 7: [3, 11, 6] },
+      { 11: [10, 7, 15] },
+      { 13: [12, 14, 9] },
+      { 14: [13, 15, 10] },
+      { 5: [4, 6, 1, 9] },
+      { 6: [5, 7, 2, 10] },
+      { 9: [8, 10, 5, 13] },
+      { 10: [9, 11, 6, 14] },
+    ];
+    let possibleEmptyCoordinates = allowedMoveCoordinates[tileCoordinate];
+    if (possibleEmptyCoordinates.includes(emptyTileCoordinate)) {
+      return true;
+    }
+    return false;
+  };
 
   return (
     <div className="App">
